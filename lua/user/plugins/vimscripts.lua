0439d63e5bfc5e506a8e6de423eed1257011db0c
Armin Veres <armin.veres@nse.ch>
2023-02-03 12:29:25 +0100

    Added lazyloading to some vimscript ft plugins

--- lua/user/plugins/vimscripts.lua
+++ lua/user/plugins/vimscripts.lua
@@ -1,14 +1,13 @@
 return {
+  { "lervag/vimtex", ft = "tex" },
+  { "fladson/vim-kitty", ft = "kitty" },
+  { "ron-rs/ron.vim", ft = "ron" },
+  { "mboughaba/i3config.vim", ft = "i3config" },
   "RRethy/vim-illuminate", -- illuminate word under cursor
   "tpope/vim-sleuth", -- automatically adjusts 'shiftwidth' and 'expandtab' based on the current file
   "ludovicchabant/vim-gutentags", -- Automatic tags management
   "moll/vim-bbye", -- allows you to do delete buffers (close files) without closing your windows
   "mbbill/undotree",
-  "fladson/vim-kitty",
-  "ron-rs/ron.vim",
-  "lervag/vimtex",
-  "mphe/grayout.vim",
-  -- "mboughaba/i3config.vim",
   -- 'tpope/vim-fugitive',                     -- Git commands in nvim
   -- 'tpope/vim-rhubarb',                      -- Fugitive-companion to interact with github
 }
