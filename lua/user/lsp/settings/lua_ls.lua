b77869bbbc2e5ef16963e01e5be373b238f55103
Armin Veres <armin.veres@hotmail.com>
2023-02-13 11:53:23 +0100

    Added nvim runtime files and disabled 3rd party libs

--- lua/user/lsp/settings/lua_ls.lua
+++ lua/user/lsp/settings/lua_ls.lua
@@ -11,12 +11,9 @@ return {
         -- globals = { "vim", "packer_plugins", "awesome", "client", "root", "use" },
       },
       workspace = {
-        library = {
-          -- Make the server aware of Neovim runtime files
-          -- [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          -- [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-          -- ["/usr/share/awesome/lib"] = true,
-        },
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
       },
       -- Do not send telemetry data containing a randomized but unique identifier
       telemetry = {
