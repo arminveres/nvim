ca4ebb8f7ea39a1b7be0fe89b875c0487b7b5714
Armin Veres <armin.veres@hotmail.com>
2023-05-21 14:22:58 +0200

    feat: md and tex after mappings

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,2 +1,5 @@
-local opts = { silent = true, remap = false }
-vim.keymap.set("n", "<leader>m", [[:MarkdownPreview<CR>]], opts) -- open markdown preview
+-- local opts = { silent = true, remap = false }
+-- vim.keymap.set("n", "<leader>m", [[:MarkdownPreview<CR>]], opts) -- open markdown preview
+vim.keymap.set("n", "<leader>m", ":MarkdownPreview<CR>", {
+    desc = "Preview markdown file in browser"
+}) -- open markdown preview
---
+++ after/ftplugin/tex.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>vc", ":VimtexCompile<CR>", { desc = "Compile and preview TeX file" }) -- open markdown preview
+vim.keymap.set("n", "<leader>vv", ":VimtexView<CR>", { desc = "Open preview TeX file" }) -- open markdown preview
