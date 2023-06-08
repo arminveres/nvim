2faeb8825dea84d89b2011f1bca5fa0e2550978d
Armin Veres <armin.veres@hotmail.com>
2023-06-08 18:31:47 +0200

    feat: add md-pdf to rtp for development

--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
 require("user.options")
 require("user.gui-options") -- Needs to be loaded before the plugins, otherwise shell does not get recognized for WIN Gui
-require("user.mappings") -- NOTE: could move this to bottom to make all mappings configurable there.
+require("user.mappings")    -- NOTE: could move this to bottom to make all mappings configurable there.
 require("user.lazy")
 require("user.lsp")
 require("user.autocmds")
 
 vim.opt.showtabline = 1 -- only show when more than one 1 tab, need to be loaded last, gets overwritten otherwise
 vim.cmd([[highlight link TrailingSpaces Error | match TrailingSpaces /\s\+$/]])
+
+vim.opt.runtimepath:append(", ~/gitfiles/md-pdf.nvim")
