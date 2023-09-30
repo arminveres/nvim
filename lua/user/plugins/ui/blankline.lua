52df5b152c89b1da26ca5c4a506de399e4094bd0
Armin Veres <armin.veres@hotmail.com>
2023-09-30 12:34:50 +0200

    fix: lualine and indentline

--- lua/user/plugins/ui/blankline.lua
+++ lua/user/plugins/ui/blankline.lua
@@ -1,12 +1,13 @@
 return {
-  "lukas-reineke/indent-blankline.nvim", -- Add indentation guides even on blank lines
-  lazy = true,
-  event = 'BufEnter',
-  opts = {
-    -- char = "┊",
-    -- show_trailing_blankline_indent = false,
-    show_current_context_start = true,
-    show_current_context = true,
-  },
-  config = true,
+    "lukas-reineke/indent-blankline.nvim", -- Add indentation guides even on blank lines
+    main = "ibl",
+    lazy = true,
+    event = "BufEnter",
+    opts = {
+        -- char = "┊",
+        -- show_trailing_blankline_indent = false,
+        show_current_context_start = true,
+        show_current_context = true,
+    },
+    config = true,
 }
--- lua/user/plugins/ui/lualine.lua
+++ lua/user/plugins/ui/lualine.lua
@@ -1,19 +1,19 @@
 local function get_active_lsps()
-    local msg = 'No Active Lsp'
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    local msg = "No Active Lsp"
+    local buf_ft = vim.api.nvim_get_option_value("filetype", { buf = 0 })
     local clients = vim.lsp.get_clients()
     if next(clients) == nil then
         return msg
     end
-    local lsps = ''
+    local lsps = ""
     for _, client in ipairs(clients) do
         local filetypes = client.config.filetypes
         if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-            lsps = lsps .. client.name .. ' '
+            lsps = lsps .. client.name .. " "
         end
     end
 
-    if lsps == '' then
+    if lsps == "" then
         return msg
     else
         return lsps
@@ -25,7 +25,7 @@ local function get_inactive_winbar()
 end
 
 local function get_active_winbar()
-    local bar = require('lspsaga.symbol.winbar').get_bar()
+    local bar = require("lspsaga.symbol.winbar").get_bar()
     if not bar then
         return get_inactive_winbar()
     end
@@ -34,40 +34,40 @@ end
 
 return {
     {
-        'nvim-lualine/lualine.nvim', -- Fancier statusline
+        "nvim-lualine/lualine.nvim", -- Fancier statusline
         config = function()
-            local my_theme = require('lualine.themes.powerline')
-            local gruvbox = require('gruvbox.palette').colors
+            local my_theme = require("lualine.themes.powerline")
+            local gruvbox_colors = require("gruvbox.palette").get_base_colors({}, nil, "")
             local my_colors = {
-                gray1 = '#262626',
-                gray2 = '#303030',
-                gray4 = '#585858',
-                gray5 = '#606060',
-                gray7 = '#9e9e9e',
-                gray8 = '#aaaaaa',
-                gray10 = '#f0f0f0',
+                gray1 = "#262626",
+                gray2 = "#303030",
+                gray4 = "#585858",
+                gray5 = "#606060",
+                gray7 = "#9e9e9e",
+                gray8 = "#aaaaaa",
+                gray10 = "#f0f0f0",
             }
 
             my_theme.normal = {
                 -- a = { fg = gruvbox.dark0, bg = gruvbox.bright_orange, gui = 'bold' },
-                a = { fg = gruvbox.dark0, bg = '#d75f00', gui = 'bold' },
-                b = { fg = my_colors.gray10, bg = gruvbox.dark1 },
-                c = { fg = my_colors.gray8, bg = gruvbox.dark0 },
+                a = { fg = gruvbox_colors.dark0, bg = "#d75f00", gui = "bold" },
+                b = { fg = my_colors.gray10, bg = gruvbox_colors.dark1 },
+                c = { fg = my_colors.gray8, bg = gruvbox_colors.dark0 },
             }
             my_theme.visual = {
-                a = { fg = gruvbox.light0, bg = gruvbox.neutral_blue, gui = 'bold' },
+                a = { fg = gruvbox_colors.light0, bg = gruvbox_colors.neutral_blue, gui = "bold" },
             }
             my_theme.insert = {
-                a = { fg = gruvbox.dark0, bg = gruvbox.neutral_green, gui = 'bold' },
+                a = { fg = gruvbox_colors.dark0, bg = gruvbox_colors.neutral_green, gui = "bold" },
             }
             my_theme.terminal = {
-                a = { fg = gruvbox.dark0, bg = gruvbox.bright_aqua, gui = 'bold' },
+                a = { fg = gruvbox_colors.dark0, bg = gruvbox_colors.bright_aqua, gui = "bold" },
             }
-            my_theme.inactive.a.fg = gruvbox.light0_hard
-            my_theme.inactive.b.fg = gruvbox.light0_hard
-            my_theme.inactive.c.fg = gruvbox.light0_hard
+            my_theme.inactive.a.fg = gruvbox_colors.light0_hard
+            my_theme.inactive.b.fg = gruvbox_colors.light0_hard
+            my_theme.inactive.c.fg = gruvbox_colors.light0_hard
 
-            require('lualine').setup({
+            require("lualine").setup({
                 options = {
                     icons_enabled = true,
                     theme = my_theme, -- "powerline", --"adwaita", "auto"
@@ -78,31 +78,31 @@ return {
                     globalstatus = true,
                 },
                 sections = {
-                    lualine_a = { { 'mode' } },
+                    lualine_a = { { "mode" } },
                     lualine_b = {
-                        { 'branch', icon = { '', color = { fg = '#f34f29' } } },
-                        { 'diff' },
-                        { 'diagnostics' },
+                        { "branch", icon = { "", color = { fg = "#f34f29" } } },
+                        { "diff" },
+                        { "diagnostics" },
                     },
                     lualine_c = {
-                        { '%=', separator = '' },
+                        { "%=", separator = "" },
                         {
                             function()
                                 return vim.fn.fnamemodify(
-                                    require('project_nvim.project').find_pattern_root(),
-                                    ':t'
+                                    require("project_nvim.project").find_pattern_root(),
+                                    ":t"
                                 )
                             end,
                             cond = function()
                                 local capture = vim.fn.fnamemodify(
-                                    require('project_nvim.project').find_pattern_root(),
-                                    ':t'
+                                    require("project_nvim.project").find_pattern_root(),
+                                    ":t"
                                 )
-                                return capture ~= 'null' and capture ~= 'v:null'
+                                return capture ~= "null" and capture ~= "v:null"
                             end,
                         },
                         {
-                            'filename',
+                            "filename",
                             path = 0, -- 0: Just the filename, 1: Relative path, 2: Absolute path
                             file_status = true, -- Displays file status (readonly status, modified status)
                             shorting_target = 40, -- Shortens path to leave 40 spaces in the window for other components.
@@ -111,20 +111,20 @@ return {
                     lualine_x = {
                         {
                             get_active_lsps,
-                            icon = '  LSP:',
+                            icon = "  LSP:",
                         },
-                        'encoding',
-                        'fileformat',
-                        'filetype',
+                        "encoding",
+                        "fileformat",
+                        "filetype",
                     },
-                    lualine_y = { 'progress' },
-                    lualine_z = { 'location' },
+                    lualine_y = { "progress" },
+                    lualine_z = { "location" },
                 },
                 inactive_sections = {
                     lualine_a = { get_inactive_winbar },
                     lualine_b = {},
-                    lualine_c = { 'filename' },
-                    lualine_x = { 'location' },
+                    lualine_c = { "filename" },
+                    lualine_x = { "location" },
                     lualine_y = {},
                     lualine_z = {},
                 },
@@ -141,5 +141,8 @@ return {
                 -- },
             })
         end,
+        dependencies = {
+            "ellisonleao/gruvbox.nvim",
+        },
     },
 }
