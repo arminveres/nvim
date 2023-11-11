582ca26854d1eb98b714bb7a9a71d042ff513afb
Armin Veres <armin.veres@hotmail.com>
2023-11-11 16:18:50 +0100

    fix(ufo): attaches at BufEnter

--- lua/user/plugins/ufo.lua
+++ lua/user/plugins/ufo.lua
@@ -1,9 +1,8 @@
 return {
     "kevinhwang91/nvim-ufo",
-    event = "BufReadPre",
     config = function()
         vim.o.foldcolumn = "1" -- '0' is not bad
-        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
 
