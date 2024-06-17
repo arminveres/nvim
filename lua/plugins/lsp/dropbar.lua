622674ea33dccbdb620c1052533a04fc64584f56
Armin Veres <armin.veres@hotmail.com>
2024-06-17 19:57:46 +0200

    feat: dropbar add keybind

--- lua/plugins/lsp/dropbar.lua
+++ lua/plugins/lsp/dropbar.lua
@@ -1,6 +1,17 @@
 return {
     "Bekaboo/dropbar.nvim",
+    keys = {
+        {
+            "<leader>o",
+            function()
+                require("dropbar.api").pick()
+            end,
+        },
+    },
     opts = {},
     -- optional, but required for fuzzy finder support
-    -- dependencies = "nvim-telescope/telescope-fzf-native.nvim",
+    dependencies = {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+    },
 }
