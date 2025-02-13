83e620a950989c735e63e76b451c1023a9aada2c
Armin Richard Veres <arminrichard.veres@varian.com>
2025-02-13 10:18:20 +0100

    doc: add descriptions to gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -17,7 +17,7 @@ local gs_opts = {
                 gs.next_hunk()
             end)
             return "<Ignore>"
-        end, { expr = true })
+        end, { expr = true, desc = "Next Git Change" })
 
         map("n", "[c", function()
             if vim.wo.diff then
@@ -27,29 +27,34 @@ local gs_opts = {
                 gs.prev_hunk()
             end)
             return "<Ignore>"
-        end, { expr = true })
+        end, { expr = true, desc = "Previous Git Change" })
 
         -- Actions
-        map("n", "<leader>hs", gs.stage_hunk)
-        map("n", "<leader>hr", gs.reset_hunk)
+        map("n", "<leader>hs", gs.stage_hunk, { desc = "GitSigns [h]unk [s]tage" })
+        map("n", "<leader>hr", gs.reset_hunk, { desc = "GitSigns [h]unk [r]eset" })
         map("v", "<leader>hs", function()
             gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end)
+        end, { desc = "GitSigns [h]unk [s]tage" })
         map("v", "<leader>hr", function()
             gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end)
-        map("n", "<leader>hS", gs.stage_buffer)
-        map("n", "<leader>hu", gs.undo_stage_hunk)
-        map("n", "<leader>hR", gs.reset_buffer)
+        end, { desc = "GitSigns [h]unk [r]eset" })
+        map("n", "<leader>hS", gs.stage_buffer, { desc = "GitSigns [h]unk (buffer) [S]tage" })
+        map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "GitSigns [h]unk [u]ndo" })
+        map("n", "<leader>hR", gs.reset_buffer, { desc = "GitSigns [h]unk (buffer) [R]eset" })
         map("n", "<leader>hp", gs.preview_hunk)
         map("n", "<leader>hb", function()
             gs.blame_line({ full = true })
-        end)
-        map("n", "<leader>tb", gs.toggle_current_line_blame)
-        map("n", "<leader>hd", gs.diffthis)
+        end, { desc = "GitSigns [h]unk [b]lame" })
+        map(
+            "n",
+            "<leader>tb",
+            gs.toggle_current_line_blame,
+            { desc = "GitSigns [t]oggle line [b]lame" }
+        )
+        map("n", "<leader>hd", gs.diffthis, { desc = "GitSigns diffthis" })
         map("n", "<leader>hD", function()
             gs.diffthis("~")
-        end)
+        end, { desc = "GitSigns diffthis ~" })
         map("n", "<leader>td", gs.toggle_deleted)
 
         -- Text object
