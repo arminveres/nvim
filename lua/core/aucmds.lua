bbc065a8218d02fc16d9b554cec9c1bf137a2eec
Armin Richard Veres <arminrichard.veres@varian.com>
2025-02-13 10:17:19 +0100

    refactor: move qmk autocmd to lazy config function

--- lua/core/aucmds.lua
+++ lua/core/aucmds.lua
@@ -42,47 +42,5 @@ aucmd("BufWritePre", {
 })
 
 
--- ================================================================================================
--- Configure QMK.nvim for multiple layouts
--- ================================================================================================
-local qmk_group = create_augroup("MyQMK", {})
-local qmk_path = "*sofle/keymaps/arminveres/keymap.c"
-aucmd("BufEnter", {
-    desc = "Format simple keymap",
-    group = qmk_group,
-    pattern = qmk_path,
-    callback = function()
-        require("qmk").setup({
-            auto_format_pattern = "*sofle/keymaps/arminveres/keymap.c",
-            name = "LAYOUT",
-            layout = {
-                "x x x x x x _ _ _ x x x x x x",
-                "x x x x x x _ _ _ x x x x x x",
-                "x x x x x x _ _ _ x x x x x x",
-                "x x x x x x x _ x x x x x x x",
-                "_ _ x x x x x _ x x x x x _ _",
-            },
-        })
-    end,
-})
-
-qmk_path = "*moonlander/keymaps/arminveres/keymap.c"
-aucmd("BufEnter", {
-    desc = "Format simple keymap",
-    group = qmk_group,
-    pattern = qmk_path,
-    callback = function()
-        require("qmk").setup({
-            auto_format_pattern = qmk_path,
-            name = "LAYOUT_moonlander",
-            layout = {
-                "x x x x x x x _ x x x x x x x",
-                "x x x x x x x _ x x x x x x x",
-                "x x x x x x x _ x x x x x x x",
-                "x x x x x x _ _ _ x x x x x x",
-                "x x x x x _ x _ x _ x x x x x",
-                "_ _ _ x x x _ _ _ x x x _ _ _",
-            },
-        })
     end,
 })
--- lua/plugins/qmk.lua
+++ lua/plugins/qmk.lua
@@ -2,4 +2,52 @@ return {
     -- NOTE: See further config at lua/user/autocmds.lua
     "codethread/qmk.nvim",
     event = "BufEnter *arminveres/keymap.c",
+    config = function()
+        -- ================================================================================================
+        -- Configure QMK.nvim for multiple layouts
+        -- ================================================================================================
+        local aucmd = vim.api.nvim_create_autocmd
+        local create_augroup = vim.api.nvim_create_augroup
+        local qmk_group = create_augroup("MyQMK", {})
+        local qmk_path = "*sofle/keymaps/arminveres/keymap.c"
+        aucmd("BufEnter", {
+            desc = "Format simple keymap",
+            group = qmk_group,
+            pattern = qmk_path,
+            callback = function()
+                require("qmk").setup({
+                    auto_format_pattern = "*sofle/keymaps/arminveres/keymap.c",
+                    name = "LAYOUT",
+                    layout = {
+                        "x x x x x x _ _ _ x x x x x x",
+                        "x x x x x x _ _ _ x x x x x x",
+                        "x x x x x x _ _ _ x x x x x x",
+                        "x x x x x x x _ x x x x x x x",
+                        "_ _ x x x x x _ x x x x x _ _",
+                    },
+                })
+            end,
+        })
+
+        qmk_path = "*moonlander/keymaps/arminveres/keymap.c"
+        aucmd("BufEnter", {
+            desc = "Format simple keymap",
+            group = qmk_group,
+            pattern = qmk_path,
+            callback = function()
+                require("qmk").setup({
+                    auto_format_pattern = qmk_path,
+                    name = "LAYOUT_moonlander",
+                    layout = {
+                        "x x x x x x x _ x x x x x x x",
+                        "x x x x x x x _ x x x x x x x",
+                        "x x x x x x x _ x x x x x x x",
+                        "x x x x x x _ _ _ x x x x x x",
+                        "x x x x x _ x _ x _ x x x x x",
+                        "_ _ _ x x x _ _ _ x x x _ _ _",
+                    },
+                })
+            end,
+        })
+    end,
 }
