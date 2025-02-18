0885e073ddf4aad080083de25e30d270ca6d835d
Armin Veres <armin.veres@hotmail.com>
2025-02-18 21:35:11 +0100

    fix: todo plugin pattern parsing

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -1,15 +1,28 @@
 return {
     "folke/todo-comments.nvim",
+    lazy = false,
     keys = {
-        { "]t", function() require("todo-comments").jump_next() end, desc = "Next TODO comment" },
+        {
+            "]t",
+            function()
+                require("todo-comments").jump_next()
+            end,
+            desc = "Next TODO comment",
+        },
         {
             "[t",
-            function() require("todo-comments").jump_prev() end,
+            function()
+                require("todo-comments").jump_prev()
+            end,
             desc = "Previous TODO comment",
         },
     },
     opts = {
-        search = { pattern = "[[\b(KEYWORDS).*:?]]" },
+        search = { pattern = [[\b(KEYWORDS).*:?]] },
+        highlight = {
+            pattern = [[.*<(KEYWORDS).*:]],
+            keyword = "bg",
+        },
     },
     dependencies = { "nvim-lua/plenary.nvim" },
 }
