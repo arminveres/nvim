9c800e878f344bae589a480591ffb45e972a6989
Armin Richard Veres <arminrichard.veres@varian.com>
2025-02-24 10:13:18 +0100

    add todo-comments mapping

--- lua/plugins/todo.lua
+++ lua/plugins/todo.lua
@@ -2,20 +2,9 @@ return {
     "folke/todo-comments.nvim",
     lazy = false,
     keys = {
-        {
-            "]t",
-            function()
-                require("todo-comments").jump_next()
-            end,
-            desc = "Next TODO comment",
-        },
-        {
-            "[t",
-            function()
-                require("todo-comments").jump_prev()
-            end,
-            desc = "Previous TODO comment",
-        },
+        -- { "]t", function() require("todo-comments").jump_next() end, desc = "Next TODO comment", },
+        -- { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous TODO comment", },
+        { "<leader>st", function() Snacks.picker.todo_comments() end, desc = "Todo" },
     },
     opts = {
         search = { pattern = [[\b(KEYWORDS).*:?]] },
