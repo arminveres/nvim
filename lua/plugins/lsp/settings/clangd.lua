6ed633c7110a4e841ecca6407be7671ab09fa6ce
Armin Richard Veres <armin.veres@hotmail.com>
2025-04-17 17:37:16 +0200

    feat: create clangd command templte for .nvim.lua

--- lua/plugins/lsp/settings/clangd.lua
+++ lua/plugins/lsp/settings/clangd.lua
@@ -1,26 +1,26 @@
 local M = {}
 
--- @brief Returns the number of processing cores for clangd to use, default is 4
-local nproc = function()
+---@return number procn The number of processing cores for clangd to use.
+local function nproc()
     local procn = tonumber(vim.fn.system("nproc"))
-    if procn then
-        return procn
-    end
+    if procn then return procn end
     return 10
 end
 
+M.command_template = {
+    "clangd",
+    "--background-index",
+    "--header-insertion=iwyu", -- never
+    "--clang-tidy",
+    "-j=" .. nproc(),
+    "--header-insertion-decorators",
+    "--all-scopes-completion",
+    "--pch-storage=memory",
+    "--offset-encoding=utf-16",
+}
+
 M.server_opts = {
-    cmd = {
-        "clangd",
-        "--background-index",
-        "--header-insertion=iwyu", -- never
-        "--clang-tidy",
-        "-j=" .. nproc(),
-        "--header-insertion-decorators",
-        "--all-scopes-completion",
-        "--pch-storage=memory",
-        "--offset-encoding=utf-16",
-    },
+    cmd = M.command_template,
 }
 
 M.extensions = {}
