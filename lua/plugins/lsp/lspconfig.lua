b59e377d523a38d7443b2b388eef031ebdbd2892
Armin Richard Veres <armin.veres@hotmail.com>
2025-04-22 16:00:06 +0200

    chore: remove marksman lsp config

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -48,9 +48,6 @@ local function setup()
         elseif server == "texlab" then
             local texlab_opts = require("plugins.lsp.settings.texlab")
             lsp_opts = vim.tbl_deep_extend("force", texlab_opts, lsp_opts)
-        elseif server == "marksman" then
-            local marksman_opts = require("plugins.lsp.settings.marksman")
-            lsp_opts = vim.tbl_deep_extend("force", marksman_opts, lsp_opts)
         elseif server == "jsonls" then
             local jsonls_opts = require("plugins.lsp.settings.jsonls")
             lsp_opts = vim.tbl_deep_extend("force", jsonls_opts, lsp_opts)
