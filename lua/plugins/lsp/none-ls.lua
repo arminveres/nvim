5fc2b75b5eeb90c948416b2e866d72f4232a542e
Armin Richard Veres <armin.veres@hotmail.com>
2025-04-30 11:55:47 +0200

    fix: temporary PR branch for none-ls

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -43,7 +43,8 @@ local function setup()
 end
 
 return {
-    "nvimtools/none-ls.nvim", -- Null LS replacement
+    -- "nvimtools/none-ls.nvim", -- Null LS replacement
+    "https://github.com/ulisses-cruz/none-ls.nvim",
     event = "VeryLazy",
     config = setup,
     dependencies = {
