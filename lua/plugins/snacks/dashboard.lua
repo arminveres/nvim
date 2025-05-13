600bce047ae7b361c35632d095f1b0219f252e40
Armin Richard Veres <armin.veres@hotmail.com>
2025-05-13 14:49:45 +0200

    dashboard: disable arasaka header

--- lua/plugins/snacks/dashboard.lua
+++ lua/plugins/snacks/dashboard.lua
@@ -3,7 +3,7 @@ return {
     ---@type snacks.Config
     opts = {
         dashboard = {
-            preset = { header = require("extras.ascii-art").arasaka_ascii_m },
+            -- preset = { header = require("extras.ascii-art").arasaka_ascii_s },
             -- your dashboard configuration comes here or leave it empty to use the default settings
             -- refer to the configuration section below
         },
