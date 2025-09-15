6dc3aa856d4f1b81bfdab13aefa9434e6cada783
Armin Richard Veres <armin.veres@proton.me>
2025-09-15 21:01:43 +0200

    feat: add qmk format for cygnus

--- lua/plugins/qmk.lua
+++ lua/plugins/qmk.lua
@@ -42,6 +42,24 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end,
 })
 
+vim.api.nvim_create_autocmd("BufEnter", {
+    desc = "Format simple keymap",
+    group = qmk_group,
+    pattern = "*crkbd/rev1/keymaps/arminveres/keymap.c",
+    callback = function()
+        require("qmk").setup({
+            auto_format_pattern = "none",
+            name = "LAYOUT",
+            layout = {
+                "x x x x x _ _ _ x x x x x",
+                "x x x x x _ _ _ x x x x x",
+                "x x x x x _ _ _ x x x x x",
+                "_ _ _ x x x _ x x x _ _ _",
+            },
+        })
+    end,
+})
+
 return {
     "codethread/qmk.nvim",
     event = "BufEnter *arminveres/keymap.c",
