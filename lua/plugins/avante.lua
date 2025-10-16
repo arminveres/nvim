a6b040b5cb8298e4a4fd85a0dabc82236396f37a
Armin Richard Veres <armin.veres@proton.me>
2025-10-16 13:30:45 +0200

    cleanup: remove dead code/plugin

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,49 +1,23 @@
 return {
     "yetone/avante.nvim",
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    -- ⚠️ must add this setting! ! !
     build = vim.fn.has("win32") ~= 0
-            and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
         or "make",
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
     ---@module 'avante'
     ---@type avante.Config
     opts = {
-        -- add any opts here
-        -- this file can contain specific instructions for your project
-        instructions_file = "avante.md",
-        -- for example
         provider = "copilot",
-        providers = {
-            claude = {
-                endpoint = "https://api.anthropic.com",
-                model = "claude-sonnet-4-20250514",
-                timeout = 30000, -- Timeout in milliseconds
-                extra_request_body = {
-                    temperature = 0.75,
-                    max_tokens = 20480,
-                },
-            },
-            moonshot = {
-                endpoint = "https://api.moonshot.ai/v1",
-                model = "kimi-k2-0711-preview",
-                timeout = 30000, -- Timeout in milliseconds
-                extra_request_body = {
-                    temperature = 0.75,
-                    max_tokens = 32768,
-                },
-            },
-        },
     },
     dependencies = {
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
         --- The below dependencies are optional,
-        "folke/snacks.nvim", -- for input provider snacks
+        "folke/snacks.nvim",           -- for input provider snacks
         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
         {
-            "zbirenbaum/copilot.lua", -- for providers='copilot'
+            "zbirenbaum/copilot.lua",  -- for providers='copilot'
             opts = {},
         },
         {
