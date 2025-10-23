7fc05133cd5866a1b1550fac30f6e33d967d074d
Armin Richard Veres <armin.veres@proton.me>
2025-10-23 17:14:09 +0200

    refactor: disable clangd extension and inline clangd options

--- after/lsp/clangd.lua
+++ after/lsp/clangd.lua
@@ -1,1 +1,39 @@
-return require("plugins.lsp.settings.clangd").server_opts
+vim.keymap.set(
+    "n",
+    "gsh",
+    "<cmd>LspClangdSwitchSourceHeader<cr>",
+    { desc = "Switch to source or header" }
+)
+
+---@return number procn The number of processing cores for clangd to use.
+local function nproc()
+    local procn = tonumber(vim.fn.system("nproc"))
+    if procn then return procn end
+    return 10
+end
+
+local command_template = {
+    "clangd",
+    "--background-index",
+    "--header-insertion=iwyu", -- never
+    "--clang-tidy",
+    "-j=" .. nproc(),
+    "--header-insertion-decorators",
+    "--all-scopes-completion",
+    "--pch-storage=memory",
+    "--query-driver=**",
+}
+
+return {
+    cmd = command_template,
+    root_markers = {
+        "compile_commands.json",
+        ".clangd",
+        ".clang-tidy",
+        ".clang-format",
+        "compile_flags.txt",
+        "Makefile",
+        "configure.ac",
+        ".git",
+    },
+}
