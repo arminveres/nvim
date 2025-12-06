1244fd27f0c69418cb5537a97975371fe94d1362
Armin Richard Veres <armin.veres@proton.me>
2025-12-06 17:10:24 +0100

    feat(rust): add live diagnostics to rustaceanvim

--- lua/plugins/lsp/rustacean.lua
+++ lua/plugins/lsp/rustacean.lua
@@ -1,5 +1,20 @@
+vim.g.rustaceanvim = {
+    server = {
+        default_settings = {
+            ["rust-analyzer"] = {
+                diagnostics = {
+                    enable = true,     -- Enable live diagnostics
+                    experimental = {
+                        enable = true, -- Enable experimental features
+                    },
+                },
+            },
+        },
+    },
+}
+
 return {
     "mrcjkb/rustaceanvim",
     version = "^5", -- Recommended
-    lazy = false, -- This plugin is already lazy
+    lazy = false,   -- This plugin is already lazy
 }
