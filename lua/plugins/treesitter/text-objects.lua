6344e46da2569f680f51311f24325bc896ac78ad
Armin Richard Veres <armin.veres@proton.me>
2026-01-12 16:38:51 +0100

    fix(treesitter): remove duplicated keymaps

--- lua/plugins/treesitter/text-objects.lua
+++ lua/plugins/treesitter/text-objects.lua
@@ -31,16 +31,6 @@ return {
         )
         vim.keymap.set(
             { "x", "o" },
-            "af",
-            function() select.select_textobject("@function.outer", "textobjects") end
-        )
-        vim.keymap.set(
-            { "x", "o" },
-            "if",
-            function() select.select_textobject("@function.inner", "textobjects") end
-        )
-        vim.keymap.set(
-            { "x", "o" },
             "ac",
             function() select.select_textobject("@class.outer", "textobjects") end
         )
