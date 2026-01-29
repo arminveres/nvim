115b4e4caa9f36ee6a43a1153a33e7e24fc34ef4
Armin Richard Veres <armin.veres@proton.me>
2026-01-29 17:34:40 +0100

    feat: readd refactoring.nvim and ts-cpp-tools

---
+++ lua/plugins/treesitter/refactoring.lua
@@ -0,0 +1,18 @@
+-- BUG: does not run, causes segfault or other crash
+return {
+    "ThePrimeagen/refactoring.nvim",
+    lazy = false,
+    opts = {},
+    keys = {
+        {
+            mode = { "n", "x" },
+            "<leader>rr",
+            function() require("refactoring").select_refactor() end,
+            desc = "Refactor select",
+        },
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+    },
+}
