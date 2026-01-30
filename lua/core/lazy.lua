6e6e33bea3183eb3701c0a8b13b0e78d12ad6cf7
Armin Richard Veres <armin.veres@proton.me>
2026-01-30 16:27:46 +0100

    feat: add lazy keymap

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -13,7 +13,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
             { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
+            { out, "WarningMsg" },
             { "\nPress any key to exit..." },
         }, true, {})
         vim.fn.getchar()
@@ -53,3 +53,5 @@ require("lazy").setup({
         },
     },
 })
+
+vim.keymap.set("n", "<leader>ll","<cmd>Lazy<cr>", { desc = "Open Lazy" })
