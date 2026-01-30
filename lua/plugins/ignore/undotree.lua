566b3f290df09e1a19799a1237c5ba3e80b0beda
Armin Richard Veres <armin.veres@proton.me>
2026-01-30 17:10:41 +0100

    cleanup: remove undotree

--- lua/plugins/ignore/undotree.lua
+++ lua/plugins/ignore/undotree.lua
@@ -1,7 +1,5 @@
 return {
-    enabled = false, -- superceded by Snacks.picker
     "mbbill/undotree",
-    lazy = false,
     cmd = "UndotreeToggle",
     keys = {
         { "<Leader>u", "<Cmd>UndotreeToggle<CR>", desc = "Toggle the [u]ndo tree" },
