3e1d763ed2f2b724fbe0ab78720f0a7cea889267
Armin Richard Veres <armin.veres@proton.me>
2026-01-30 16:29:06 +0100

    cleanup: ufo

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,7 +1,7 @@
 return {
     "kevinhwang91/nvim-ufo",
     event = "VeryLazy",
-    config = function()
+    init = function()
         -- use Neovim nightly branch
         vim.o.fillchars = "eob: ,fold: ,foldopen:,foldsep: ,foldinner: ,foldclose:"
 
@@ -9,13 +9,10 @@ return {
         vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
-
-        require("ufo").setup({
-            provider_selector = function(bufnr, filetype, buftype)
-                return { "treesitter", "indent" }
-            end,
-        })
     end,
+    opts = {
+        provider_selector = function(bufnr, filetype, buftype) return { "treesitter", "indent" } end,
+    },
     dependencies = {
         "kevinhwang91/promise-async",
         "nvim-treesitter/nvim-treesitter",
