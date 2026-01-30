26a07882eea2fd6fba3e165b2ff68ee4483fd085
Armin Richard Veres <armin.veres@proton.me>
2026-01-30 15:29:53 +0100

    refactor: move context-comments location

--- lua/plugins/treesitter/context-comment.lua
+++ lua/plugins/treesitter/context-comment.lua
@@ -1,12 +1,11 @@
 return {
     "JoosepAlviste/nvim-ts-context-commentstring", -- better context aware commenting
+    -- event = "VeryLazy",
+    keys = { "gc" },
+    opts = {},
+    init = function() vim.g.skip_ts_context_commentstring_module = true end,
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
         "numToStr/Comment.nvim",
     },
-    event = "VeryLazy",
-    opts = {},
-    init = function()
-        vim.g.skip_ts_context_commentstring_module = true
-    end,
 }
