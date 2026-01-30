6453a22f7b17237aa31e0017c9e6075e781b1235
Armin Richard Veres <armin.veres@proton.me>
2026-01-30 14:39:54 +0100

    refactor: move lazydev config to blink

--- lua/plugins/completion/blink.cmp.lua
+++ lua/plugins/completion/blink.cmp.lua
@@ -1,76 +1,104 @@
 return {
-    "saghen/blink.cmp",
-    event = "InsertEnter",
+    {
+        "saghen/blink.cmp",
+        event = "InsertEnter",
 
-    -- use a release tag to download pre-built binaries
-    version = "1.*",
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
+        -- use a release tag to download pre-built binaries
+        version = "1.*",
+        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+        -- build = 'cargo build --release',
+        -- If you use nix, you can build from source using latest nightly rust with:
+        -- build = 'nix run .#build-plugin',
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-        -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-        -- 'super-tab' for mappings similar to vscode (tab to accept)
-        -- 'enter' for enter to accept
-        -- 'none' for no mappings
-        --
-        -- All presets have the following mappings:
-        -- C-space: Open menu or open docs if already open
-        -- C-n/C-p or Up/Down: Select next/previous item
-        -- C-e: Hide menu
-        -- C-k: Toggle signature help (if signature.enabled = true)
-        --
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = "enter" },
-        -- keymap = { preset = "super-tab", ["<Tab>"] = { function(cmp) if vim.b[vim.api.nvim_get_current_buf()].nes_state then cmp.hide() return ( require("copilot-lsp.nes").apply_pending_nes() and require("copilot-lsp.nes").walk_cursor_end_edit()) end if cmp.snippet_active() then return cmp.accept() else return cmp.select_and_accept() end end, "snippet_forward", "fallback", }, },
-
-        appearance = {
-            -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-            -- Adjusts spacing to ensure icons are aligned
-            nerd_font_variant = "mono",
-        },
-
-        completion = {
-            -- (Default) Only show the documentation popup when manually triggered
-            documentation = { auto_show = true },
-            -- do not select the first entry
-            list = { selection = { preselect = false, auto_insert = false } },
-        },
-
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
-        sources = {
-            default = {
-                "lsp",
-                "path",
-                "buffer",
-                -- "copilot",
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+            -- 'super-tab' for mappings similar to vscode (tab to accept)
+            -- 'enter' for enter to accept
+            -- 'none' for no mappings
+            --
+            -- All presets have the following mappings:
+            -- C-space: Open menu or open docs if already open
+            -- C-n/C-p or Up/Down: Select next/previous item
+            -- C-e: Hide menu
+            -- C-k: Toggle signature help (if signature.enabled = true)
+            --
+            -- See :h blink-cmp-config-keymap for defining your own keymap
+            keymap = {
+                preset = "enter",
+                --[[ ["<Tab>"] = {
+                function(cmp)
+                    local col = vim.fn.col(".")
+                    local line = vim.fn.getline(".")
+                    local char = line:sub(col , col + 1)
+                    vim.notify("col: " .. tostring(col))
+                    vim.notify("line: " .. tostring(line))
+                    vim.notify("char: " .. char        )
+                    if char:match("%)") then
+                        vim.notify("hi")
+                        return "<Right>"
+                    else
+                        if cmp.snippet_active() then
+                            return cmp.accept()
+                        else
+                            return cmp.select_and_accept()
+                        end
+                    end
+                end,
+                "snippet_forward",
+                "fallback",
+            }, ]]
             },
-            providers = {
-                -- defaults to `{ 'buffer' }`
-                lsp = { fallbacks = {} },
-                copilot = {
-                    name = "copilot",
-                    module = "blink-copilot",
-                    score_offset = 100,
-                    async = true,
+
+            appearance = {
+                -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = "mono",
+            },
+
+            completion = {
+                -- (Default) Only show the documentation popup when manually triggered
+                documentation = { auto_show = true },
+                -- do not select the first entry
+                list = { selection = { preselect = false, auto_insert = false } },
+            },
+
+            -- Default list of enabled providers defined so that you can extend it
+            -- elsewhere in your config, without redefining it, due to `opts_extend`
+            sources = {
+                default = { "lazydev", "lsp", "path", "buffer" },
+                providers = {
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+                        -- make lazydev completions top priority (see `:h blink.cmp`)
+                        score_offset = 100,
+                    },
                 },
             },
+
+            -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+            --
+            -- See the fuzzy documentation for more information
+            fuzzy = { implementation = "prefer_rust_with_warning" },
+
+            -- let cmdline completions be handled by native cmdline completion.
+            cmdline = { enabled = false },
         },
-
-        -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-        -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-        -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-        --
-        -- See the fuzzy documentation for more information
-        fuzzy = { implementation = "prefer_rust_with_warning" },
-
-        -- let cmdline completions be handled by native cmdline completion.
-        cmdline = { enabled = false },
+        opts_extend = { "sources.default" },
     },
-    opts_extend = { "sources.default" },
-    dependencies = { "fang2hou/blink-copilot" },
+    {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
 }
