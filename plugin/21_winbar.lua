32b59c98cf3edda242e9da24493d401dd5b08789
Armin Richard Veres <armin.veres@proton.me>
2026-02-11 17:42:59 +0100

    fix(winbar): properly update from WinEnter only

--- plugin/21_winbar.lua
+++ plugin/21_winbar.lua
@@ -3,31 +3,30 @@ local reset = "%#Normal#"
 -- Create an autocommand group for winbar customization
 local group = vim.api.nvim_create_augroup("WinbarCustomization", { clear = true })
 
--- Set active winbar on entering a window
+local function is_float_win(win_id)
+    local win_config = vim.api.nvim_win_get_config(win_id)
+    if win_config.relative ~= "" then return true end
+    return false
+end
+
+local function inactive_winbar(win)
+    local winnr = vim.api.nvim_win_get_number(win)
+    return reset .. (" [%d] %%f"):format(winnr)
+end
+
 vim.api.nvim_create_autocmd("WinEnter", {
     group = group,
     callback = function()
-        -- do not change the winbar if not loaded
-        if not package.loaded["dropbar"] then return end
+        local cur_win_id = vim.api.nvim_get_current_win()
+        -- If we're currently in a float, don't change anything.
+        if is_float_win(cur_win_id) then return end
 
-        local current_win = vim.api.nvim_get_current_win()
-        local win_config = vim.api.nvim_win_get_config(current_win)
+        for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+            if vim.api.nvim_win_is_valid(win) and not is_float_win(win) then
+                vim.wo[win].winbar = inactive_winbar(win)
+            end
+        end
 
-        if win_config.relative ~= "" then return end
-
-        vim.wo.winbar = "%!v:lua.dropbar()"
-    end,
-})
-
--- Set inactive winbar on leaving a window
-vim.api.nvim_create_autocmd("WinLeave", {
-    group = group,
-    callback = function()
-        local current_win = vim.api.nvim_get_current_win()
-        local win_config = vim.api.nvim_win_get_config(current_win)
-
-        if win_config.relative ~= "" then return end
-
-        vim.wo.winbar = reset .. string.format(" [%d] %s", vim.api.nvim_win_get_number(0), "%f")
+        if package.loaded["dropbar"] then vim.wo[cur_win_id].winbar = "%!v:lua.dropbar()" end
     end,
 })
