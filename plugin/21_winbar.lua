808df2b670bccdc02c94dc3d478799b16e4680a7
Armin Richard Veres <armin.veres@proton.me>
2026-02-11 10:42:35 +0100

    feat(winbar): enable winbar with dropbar

--- plugin/21_winbar.lua
+++ plugin/21_winbar.lua
@@ -1,6 +1,3 @@
--- TODO(aver): Leave empty for now, but consider populating once only dropbar is not enough.
---
---[[
 local reset = "%#Normal#"
 
 -- Create an autocommand group for winbar customization
@@ -10,12 +7,14 @@ local group = vim.api.nvim_create_augroup("WinbarCustomization", { clear = true 
 vim.api.nvim_create_autocmd("WinEnter", {
     group = group,
     callback = function()
+        -- do not change the winbar if not loaded
+        if not package.loaded["dropbar"] then return end
+
         local current_win = vim.api.nvim_get_current_win()
         local win_config = vim.api.nvim_win_get_config(current_win)
 
         if win_config.relative ~= "" then return end
 
-        -- vim.wo.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
         vim.wo.winbar = "%{%v:lua.dropbar()%}"
     end,
 })
@@ -32,4 +31,3 @@ vim.api.nvim_create_autocmd("WinLeave", {
         vim.wo.winbar = reset .. string.format(" [%d] %s", vim.api.nvim_win_get_number(0), "%f")
     end,
 })
-]]
