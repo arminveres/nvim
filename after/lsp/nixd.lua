8e3678afc3c2baed4740a07c4eefcb0590fcfe2a
Armin Richard Veres <armin.veres@proton.me>
2026-02-13 19:16:05 +0100

    fix(lsp): nixd options

--- after/lsp/nixd.lua
+++ after/lsp/nixd.lua
@@ -13,14 +13,14 @@ return {
                         .. ".options",
                 },
                 home_manager = {
-                    expr = '(builtins.getFlake "/home/arminveres/nix-conf").homeConfigurations.'
-                        .. "arminveres@"
-                        .. vim.uv.os_gethostname()
-                        .. ".options",
+                    -- nixos with hm module
+                    expr =
+                    '(builtins.getFlake (builtins.toString "/home/arminveres/nix-conf")).nixosConfigurations."nixos-desktop".options.home-manager.users.type.getSubOptions []',
+                    -- hm styl
+                    -- expr = '(builtins.getFlake "/home/arminveres/nix-conf").homeConfigurations.arminveres@' .. vim.uv.os_gethostname() .. ".options",
                 },
-                flake_parts = {
-                    expr = 'let flake = builtins.getFlake ("/home/arminveres/nix-conf"); in flake.debug.options // flake.currentSystem.options',
-                },
+                -- currently not used
+                -- flake_parts = { expr = 'let flake = builtins.getFlake ("/home/arminveres/nix-conf"); in flake.debug.options // flake.currentSystem.options', },
             },
         },
     },
