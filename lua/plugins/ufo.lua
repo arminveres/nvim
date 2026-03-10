7642220384c196aa3f9d50c9d493ef436989fad4
Armin Richard Veres <armin.veres@proton.me>
2026-03-10 10:30:41 +0100

    fix(ufo): incorrect keymap setup

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,19 +1,27 @@
 return {
     "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
     init = function()
         -- use Neovim nightly branch
         vim.o.fillchars = "eob: ,fold: ,foldopen:,foldsep: ,foldinner: ,foldclose:"
 
         vim.o.foldcolumn = "1" -- '0' is not bad
-        vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
+
+        -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+        vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+        vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+        vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+        vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+        vim.keymap.set("n", "zm", require("ufo").closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+        vim.keymap.set("n", "K", function()
+            local winid = require("ufo").peekFoldedLinesUnderCursor()
+            if not winid then vim.lsp.buf.hover() end
+        end)
     end,
     opts = {
-        provider_selector = function(bufnr, filetype, buftype) return { "treesitter", "indent" } end,
-    },
-    dependencies = {
-        "kevinhwang91/promise-async",
-        -- "nvim-treesitter/nvim-treesitter", -- not really needed
+        provider_selector = function(_, _, _) return { "treesitter", "indent" } end,
     },
 }
