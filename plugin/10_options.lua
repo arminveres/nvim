44bd2c509048eab061079d9eeebd370ac8f98bce
Armin Richard Veres <armin.veres@hotmail.com>
2026-03-12 17:23:51 +0100

    feat(opts): use vim.o instead of vim.opt

--- plugin/10_options.lua
+++ plugin/10_options.lua
@@ -1,93 +1,92 @@
-vim.opt.ignorecase = true
-vim.opt.smartcase = true --Case insensitive searching UNLESS /C or capital in search
-vim.opt.smarttab = true
-vim.opt.hlsearch = true -- toggle off until next search w/ <BS>
-vim.opt.backup = true
-vim.opt.writebackup = true
-vim.opt.breakindent = true
-vim.opt.inccommand = "nosplit"
-vim.opt.mouse = "a"
-vim.opt.hidden = true --Do not save when switching buffers (note: this is now a default on master)
-vim.opt.scrolloff = 10 -- keeps x lines of context, scrolls otherwise
+vim.o.ignorecase = true
+vim.o.smartcase = true --Case insensitive searching UNLESS /C or capital in search
+vim.o.smarttab = true
+vim.o.hlsearch = true  -- toggle off until next search w/ <BS>
+vim.o.backup = true
+vim.o.writebackup = true
+vim.o.breakindent = true
+vim.o.inccommand = "nosplit"
+vim.o.mouse = "a"
+vim.o.hidden = true  --Do not save when switching buffers (note: this is now a default on master)
+vim.o.scrolloff = 10 -- keeps x lines of context, scrolls otherwise
 
-vim.opt.expandtab = true
-vim.opt.tabstop = 8
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.tabstop = 8
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 
-vim.opt.splitbelow = true -- force all horizontal splits to go below current window
-vim.opt.splitright = true -- force all vertical splits to go to the right of current window
-vim.opt.equalalways = true
+vim.o.splitbelow = true -- force all horizontal splits to go below current window
+vim.o.splitright = true -- force all vertical splits to go to the right of current window
+vim.o.equalalways = true
 
--- options to make the cursor better findable
-vim.opt.cursorline = true
-vim.opt.cursorcolumn = true
+--.o.ons to make the cursor better findable
+vim.o.cursorline = true
+vim.o.cursorcolumn = true
 
--- windows-local options
-vim.opt.relativenumber = true
-vim.opt.number = true
-vim.opt.colorcolumn = "100,120"
-vim.opt.wrap = true
-vim.opt.textwidth = 100
-vim.opt.clipboard = "unnamedplus"
+-- windows-local.o.ons
+vim.o.relativenumber = true
+vim.o.number = true
+vim.o.colorcolumn = "100,120"
+vim.o.wrap = true
+vim.o.textwidth = 100
+vim.o.clipboard = "unnamedplus"
 
-vim.opt.undofile = true --Save undo history
+vim.o.undofile = true --Save undo history
 
 --Decrease update time
-vim.opt.updatetime = 250
-vim.opt.signcolumn = "yes"
+vim.o.updatetime = 250
+vim.o.signcolumn = "yes"
 
-vim.opt.termguicolors = true
+vim.o.termguicolors = true
 
-vim.opt.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience
+vim.o.completeopt = "menuone,noselect" -- Set completeopt to have a better completion experience
 
-vim.opt.list = true
--- vim.opt.listchars:append("space:⋅")
--- vim.opt.listchars:append("eol:↴")
+vim.o.list = true
+-- vim.o.listchars:append("space:⋅")
+-- vim.o.listchars:append("eol:↴")
 
 -- activate spellcheck with <F11>
-vim.opt.spelllang = "en_us"
+vim.o.spelllang = "en_us"
 vim.o.wrapscan = true
 
-vim.opt.pumblend = 15 -- blend popup
-vim.opt.winblend = 15
+vim.o.pumblend = 15 -- blend popup
+vim.o.winblend = 15
 
-vim.opt.cmdheight = 1
+vim.o.cmdheight = 1
 -- configure ripgrep as grep program if available
 if vim.fn.executable("rg") then
     vim.o.grepprg = "rg --no-heading --vimgrep"
     vim.o.grepformat = "%f:%l:%c:%m"
 end
 
--- Windows specific options
+-- Windows specific.o.ons
 if vim.fn.has("win32") == 1 then
     --[[
-    vim.opt.shell = "bash.exe"
-    vim.opt.shellcmdflag = "--login -c" -- ignore '-i' for now
-    vim.opt.shellxquote = ""
+    vim.o.shell = "bash.exe"
+    vim.o.shellcmdflag = "--login -c" -- ignore '-i' for now
+    vim.o.shellxquote = ""
     ]]
-    vim.opt.shell = "pwsh"
-    vim.opt.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command"
-    vim.opt.shellquote = [[\"]]
-    vim.opt.shellxquote = ""
+    vim.o.shell = "pwsh"
+    vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+    vim.o.shellquote = [[\"]]
+    vim.o.shellxquote = ""
 
-    vim.opt.undodir = os.getenv("LOCALAPPDATA") .. "/Temp/nvim/undo//"
-    vim.opt.backupdir = os.getenv("LOCALAPPDATA") .. "/Temp/nvim/backup//"
+    vim.o.undodir = os.getenv("LOCALAPPDATA") .. "/Temp/nvim/undo//"
+    vim.o.backupdir = os.getenv("LOCALAPPDATA") .. "/Temp/nvim/backup//"
 else
-    vim.opt.shell = "/usr/bin/env zsh"
-    vim.opt.shellcmdflag = "-c"
+    vim.o.shell = "/usr/bin/env zsh"
+    vim.o.shellcmdflag = "-c"
 
-    vim.opt.undodir = os.getenv("XDG_CACHE_HOME") .. "/nvim/undo//"
-    vim.opt.backupdir = os.getenv("XDG_CACHE_HOME") .. "/nvim/backup//"
+    vim.o.undodir = os.getenv("XDG_CACHE_HOME") .. "/nvim/undo//"
+    vim.o.backupdir = os.getenv("XDG_CACHE_HOME") .. "/nvim/backup//"
 end
 
--- vim.opt.guifont = "RobotoMono Nerd Font Mono:h9"
-vim.opt.guifont = "Mononoki Nerd Font Mono:h11.5"
+vim.o.guifont = "Mononoki Nerd Font Propo:h11.5"
 
 vim.g.netrw_keepdir = 0
-vim.opt.laststatus = 3
+vim.o.laststatus = 3
 
-vim.opt.linespace = 0 -- adjust for lineheight
-vim.opt.exrc = true
+vim.o.linespace = 0 -- adjust for lineheight
+vim.o.exrc = true
 
--- vim.opt.fixeol = false -- stop adding automatic newline, relevant for DOS files
+-- vim.o.fixeol = false -- stop adding automatic newline, relevant for DOS files
