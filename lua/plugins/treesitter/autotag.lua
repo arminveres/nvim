9cfe080dfde52b92b83f47bdc9c85580c2d3e03a
Armin Richard Veres <armin.veres@proton.me>
2026-03-27 15:07:11 +0100

    fix: improve autotag comp

--- lua/plugins/treesitter/autotag.lua
+++ lua/plugins/treesitter/autotag.lua
@@ -1,5 +1,7 @@
+-- TODO(aver): xml currently kind of messes up to autorename
 return {
     "windwp/nvim-ts-autotag",
     opts = {},
-    -- ft = { "xml" }
+    -- TODO(aver): find nicer way to load on filetypes
+    ft = { "xml", "http", "xaml" },
 }
