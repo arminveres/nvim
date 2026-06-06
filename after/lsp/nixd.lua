b42f7aca2192a167c53c9a5605dfb1f714956004
Armin Richard Veres <armin.veres@proton.me>
2026-06-06 16:05:36 +0200

    refactor: move nixd system logic to flake root

--- after/lsp/nixd.lua
+++ after/lsp/nixd.lua
@@ -1,3 +1,4 @@
+-- https://nix-community.github.io/nixd/md_nixd_2docs_2configuration.html
 return {
     cmd = { "nixd", "--inlay-hints", "--semantic-tokens" },
     settings = {
@@ -6,22 +7,6 @@ return {
                 expr = 'import (builtins.getFlake "/home/arminveres/nix-conf").inputs.nixpkgs',
             },
             formatting = { command = { "nixfmt" } },
-            options = {
-                nixos = {
-                    expr = '(builtins.getFlake "/home/arminveres/nix-conf").nixosConfigurations.'
-                        .. vim.uv.os_gethostname()
-                        .. ".options",
-                },
-                home_manager = {
-                    -- nixos with hm module
-                    expr =
-                    '(builtins.getFlake (builtins.toString "/home/arminveres/nix-conf")).nixosConfigurations."nixos-desktop".options.home-manager.users.type.getSubOptions []',
-                    -- hm styl
-                    -- expr = '(builtins.getFlake "/home/arminveres/nix-conf").homeConfigurations.arminveres@' .. vim.uv.os_gethostname() .. ".options",
-                },
-                -- currently not used
-                -- flake_parts = { expr = 'let flake = builtins.getFlake ("/home/arminveres/nix-conf"); in flake.debug.options // flake.currentSystem.options', },
-            },
         },
     },
 }
