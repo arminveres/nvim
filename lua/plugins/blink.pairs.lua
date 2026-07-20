5d1052017888fb953a1da3f2bf883b38517710eb
Armin Richard Veres <armin.veres@proton.me>
2026-07-20 10:34:30 +0200

    fix: blink installation

--- lua/plugins/blink.pairs.lua
+++ lua/plugins/blink.pairs.lua
@@ -2,14 +2,8 @@
 return {
     "saghen/blink.pairs",
     version = "*", -- (recommended) only required with prebuilt binaries
-
-    -- download prebuilt binaries from github releases
-    -- dependencies = "saghen/blink.download",
-    -- OR build from source, requires nightly:
-    -- https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    build = "nix run .#build-plugin",
+    dependencies = "saghen/blink.lib",
+    build = function() require("blink.pairs").download():pwait(60000) end,
 
     --- @module 'blink.pairs'
     --- @type blink.pairs.Config
--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -40,12 +40,13 @@ local parsers = {
 vim.api.nvim_create_autocmd("User", {
     pattern = "TSUpdate",
     callback = function()
+        ---@diagnostic disable-next-line: missing-fields
         require("nvim-treesitter.parsers").tmux = {
             install_info = {
                 url = "https://github.com/Freed-Wu/tree-sitter-tmux",
                 revision = "a03e0b76998d6a08ea33d14bc11409c8996f4ea7", -- commit hash for revision to check out; HEAD if missing
                 -- optional entries:
-                branch = "main", -- only needed if different from default branch
+                branch = "main",                                       -- only needed if different from default branch
                 -- location = "parser", -- only needed if the parser is in subdirectory of a "monorepo"
                 -- generate = true, -- only needed if repo does not contain pre-generated `src/parser.c`
                 -- generate_from_json = false, -- only needed if repo does not contain `src/grammar.json` either
